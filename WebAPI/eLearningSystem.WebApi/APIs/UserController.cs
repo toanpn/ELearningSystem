258a51786af9f9371005991d5d3cccca3dfc7dd6
toanpn <toanphamhsgs@gmail.com>
2019-11-11 10:14:18 +0700

    config

---
+++ WebAPI/eLearningSystem.WebApi/APIs/UserController.cs
@@ -0,0 +1,27 @@
+﻿using eLearningSystem.Repositories.UnitOfWork;
+using eLearningSystem.Services.IService;
+using eLearningSystem.Services.Service;
+using System.Web.Http;
+
+namespace eLearningSystem.WebApi.APIs
+{
+    public class UserController : ApiController
+    {
+        //private IUserRepository _repository;
+        private readonly IUserService service;
+
+        private readonly UnitOfWork uow;
+
+        public UserController()
+        {
+            uow = new UnitOfWork();
+            this.service = new UserService(uow);
+        }
+        
+        // GET: api/User
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(new { results = 12 });
+        }
+    }
+}
