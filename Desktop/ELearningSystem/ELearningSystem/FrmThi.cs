e38b02eac3e431b3150749921e4c774a87354701
IquestomagSamson <36733127+IquestomagSamson@users.noreply.github.com>
2019-12-15 23:54:15 +0700

    FrmThi

---
+++ Desktop/ELearningSystem/ELearningSystem/FrmThi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ELearningSystem
+{
+    public partial class FrmThi : Form
+    {
+        public FrmThi()
+        {
+            InitializeComponent();
+        }
+    }
+}
